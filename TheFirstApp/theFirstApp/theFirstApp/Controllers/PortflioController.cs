1c206f8aa9c099203b2329ce7b8fafccf204284c
Phong3710 <nguyenthanhphong3710@gmail.com>
2021-03-17 20:03:06 +0700

    new project

---
+++ TheFirstApp/theFirstApp/theFirstApp/Controllers/PortflioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace theFirstApp.Controllers
+{
+    public class PortflioController : Controller
+    {
+        // GET: Portflio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
